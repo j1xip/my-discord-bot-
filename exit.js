f6502ea17587cfe9c25913c201a47765b7d64ea3
j1xip <gorytm52345@icloud.com>
2026-09-26 03:47:49 +0300

    Update exit.js

--- exit.js
+++ exit.js
@@ -9,7 +9,6 @@ module.exports = {
 
   async execute(interaction) {
 
-    // لازم يكون الأمر داخل سيرفر
     if (!interaction.guild) {
       return interaction.reply({
         content: '❌ هذا الأمر لازم تستخدمينه داخل سيرفر.',
@@ -17,13 +16,11 @@ module.exports = {
       });
     }
 
-    // نخلي الرد مخفي
     await interaction.reply({
       content: '👋 تم، البوت بيطلع من السيرفر.',
       ephemeral: true
     });
 
-    // نخلي البوت يطلع من السيرفر
     try {
       await interaction.guild.leave();
     } catch (error) {
